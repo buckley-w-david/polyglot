16a9e83c6f92cf27abd4be474a28b98650e9869b
David Buckley <david@davidbuckley.ca>
2022-12-06 17:38:49 -0500

    add test for missing shebang

--- tests/test_polyglot.py
+++ tests/test_polyglot.py
@@ -41,3 +41,17 @@ def test_run_failure(capfd, tmp_path):
     assert excinfo.value.exit_code == 1
     _, err = capfd.readouterr()
     assert err == "I failed!\n"
+
+NO_SHEBANG_SCRIPT = """
+print("Hello, Python!")
+"""
+
+def test_run_missing(capfd, tmp_path):
+    script = tmp_path / "test.poly"
+    script.write_text(NO_SHEBANG_SCRIPT)
+    with pytest.raises(typer.Exit) as excinfo:
+        run(script)
+
+    assert excinfo.value.exit_code == 1
+    _, err = capfd.readouterr()
+    assert err == "Mismatch in shebang lines to content blocks\nDid you forget to add a shebang line?\n"
