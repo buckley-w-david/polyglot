d8814494981344cfd4a4acea20e6d4c57ca8c1f4
David Buckley <david@davidbuckley.ca>
2022-12-06 19:09:16 -0500

    linting and typechecking

--- polyglot/cli.py
+++ polyglot/cli.py
@@ -14,16 +14,21 @@ app = typer.Typer()
 
 SHEBANG = r"^#!(/.*)"
 
+
 @app.command()
 def run(target: Path, errexit: bool = True, communicate: bool = False):
     """
     Run a polyglot script
     """
 
-    with open(target, 'r') as f:
+    with open(target, "r") as f:
         content = f.read()
 
-    scripts = [s for chunk in re.split(SHEBANG, content.strip(), 0, re.MULTILINE) if (s := chunk.strip())]
+    scripts = [
+        s
+        for chunk in re.split(SHEBANG, content.strip(), 0, re.MULTILINE)
+        if (s := chunk.strip())
+    ]
 
     # Remove "#!/usr/bin/env polyglot" (or related) if present
     if Path(sys.argv[0]).name in scripts[0]:
@@ -34,16 +39,16 @@ def run(target: Path, errexit: bool = True, communicate: bool = False):
         print("Did you forget to add a shebang line?", file=sys.stderr)
         raise typer.Exit(code=1)
 
-    in_ = b''
+    in_ = b""
     if not sys.stdin.isatty() and communicate:
         in_ = sys.stdin.read().encode()
 
     stdin, stdout, stderr = None, None, None
     if communicate:
         stdin, stdout, stderr = PIPE, PIPE, PIPE
-    
+
     for i in range(0, len(scripts), 2):
-        exe, content = scripts[i:i+2]
+        exe, content = scripts[i : i + 2]
 
         # https://stackoverflow.com/a/17371096
         fd, tmp_fpath = tempfile.mkstemp()
@@ -52,7 +57,9 @@ def run(target: Path, errexit: bool = True, communicate: bool = False):
             with open(tmp_fpath, "w") as tmp:
                 tmp.write(content)
 
-            proc = Popen([*shlex.split(exe), tmp.name], stdin=stdin, stdout=stdout, stderr=stderr)
+            proc = Popen(
+                [*shlex.split(exe), tmp.name], stdin=stdin, stdout=stdout, stderr=stderr
+            )
 
             if communicate:
                 in_, err = proc.communicate(input=in_)
@@ -62,10 +69,10 @@ def run(target: Path, errexit: bool = True, communicate: bool = False):
 
             if errexit and proc.returncode:
                 if communicate:
-                    print(err, file=sys.stderr, end='')
+                    print(err, file=sys.stderr, end="")
                 raise typer.Exit(proc.returncode)
         finally:
             os.remove(tmp_fpath)
 
     if communicate:
-        print(in_.decode(), end='')
+        print(in_.decode(), end="")
--- tests/test_polyglot.py
+++ tests/test_polyglot.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import os 
+import os
 import sys
 
 import pytest
@@ -9,6 +9,7 @@ from polyglot import __version__
 
 from polyglot.cli import run
 
+
 @pytest.fixture()
 def script(request):
     filename = request.module.__file__
@@ -16,22 +17,24 @@ def script(request):
     script = Path(test_dir) / request.param
     return script
 
-@pytest.mark.parametrize('script', ["success.poly"], indirect=True)
+
+@pytest.mark.parametrize("script", ["success.poly"], indirect=True)
 def test_run_success(script, capfd):
     sys.argv[0] = "polyglot"
     run(script)
     out, _ = capfd.readouterr()
     assert out == "Hello, Python!\nHello, Bash!\n"
 
-@pytest.mark.parametrize('script', ["communicate.poly"], indirect=True)
+
+@pytest.mark.parametrize("script", ["communicate.poly"], indirect=True)
 def test_run_communicate(script, capfd):
     sys.argv[0] = "polyglot"
     run(script, communicate=True)
     out, _ = capfd.readouterr()
-    assert out == "Hello, Bash! Also \"Hello, Python!\"\n"
+    assert out == 'Hello, Bash! Also "Hello, Python!"\n'
 
 
-@pytest.mark.parametrize('script', ["fail.poly"], indirect=True)
+@pytest.mark.parametrize("script", ["fail.poly"], indirect=True)
 def test_run_failure(script, capfd):
     sys.argv[0] = "polyglot"
     with pytest.raises(typer.Exit) as excinfo:
@@ -42,7 +45,8 @@ def test_run_failure(script, capfd):
     assert err == "I failed!\n"
     assert not out
 
-@pytest.mark.parametrize('script', ["fail.poly"], indirect=True)
+
+@pytest.mark.parametrize("script", ["fail.poly"], indirect=True)
 def test_run_failure_without_errexit(script, capfd):
     sys.argv[0] = "polyglot"
     run(script, errexit=False)
@@ -51,7 +55,8 @@ def test_run_failure_without_errexit(script, capfd):
     assert err == "I failed!\n"
     assert out == "Hello, Bash!\n"
 
-@pytest.mark.parametrize('script', ["no_shebang.poly"], indirect=True)
+
+@pytest.mark.parametrize("script", ["no_shebang.poly"], indirect=True)
 def test_run_missing(script, capfd):
     sys.argv[0] = "polyglot"
     with pytest.raises(typer.Exit) as excinfo:
@@ -59,4 +64,7 @@ def test_run_missing(script, capfd):
 
     assert excinfo.value.exit_code == 1
     _, err = capfd.readouterr()
-    assert err == "Mismatch in shebang lines to content blocks\nDid you forget to add a shebang line?\n"
+    assert (
+        err
+        == "Mismatch in shebang lines to content blocks\nDid you forget to add a shebang line?\n"
+    )
